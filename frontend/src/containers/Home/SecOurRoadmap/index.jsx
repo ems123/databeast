6c7e9d5729053e05556019756ea47364e983a87b
sitaal <gautamsital8@gmail.com>
2024-04-21 14:11:03 +0545

    updated roadmap text

--- frontend/src/containers/Home/SecOurRoadmap/index.jsx
+++ frontend/src/containers/Home/SecOurRoadmap/index.jsx
@@ -6,8 +6,8 @@ const SecOurRoadmap = ({data , img}) => {
   return (
     <section className="roadmap section-padding-100" style={{background: `url(${img}) center center / cover no-repeat`, backgroundSize: 'cover'}} id="roadmap">
         <SectionHeading
-            title='Our ICO Roadmap'
-            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis accumsan nisi Ut ut felis congue nisl hendrerit commodo.'
+            title='ROADMAP'
+            text='Roadmap and milestones of DataBeast.aigit'
         />
         <div className="container-fluid">
           <div className="row">
