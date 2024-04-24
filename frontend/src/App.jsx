bde3b589b22c1bac74e4e5b4be3428f152fc05c8
Aashish Dhakal <aashishdhakal7@gmail.com>
2024-04-24 08:42:41 +0545

    chore: add detail sample

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Aos from 'aos'
 
 import Home from './pages/Home'
+import CryptoDetail from './pages/CryptoDetail'
 
 import 'aos/dist/aos.css';
 import './assets/css/General.css';
@@ -35,7 +36,8 @@ const App = () => {
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
         </Helmet>
   			<Switch>
-          <Route path="/" exact component={Home} />        
+          <Route path="/" exact component={Home} />   
+          <Route path="/crypto-detail" exact component={CryptoDetail} />     
   			</Switch>
 	    </div>    
   );
--- frontend/src/containers/Home/SecHowItWorks/index.jsx
+++ frontend/src/containers/Home/SecHowItWorks/index.jsx
@@ -1,4 +1,6 @@
 import SectionHeading from '../../../components/SectionHeading'
+import { Link } from "react-router-dom";
+
 
 const SecHowItWorks = ({data , imgPhone}) => {
 
@@ -18,6 +20,7 @@ const SecHowItWorks = ({data , imgPhone}) => {
             <div className="services-column col-lg-6 offset-lg-0 col-md-10 offset-md-1 col-xs-10 offset-xs-1">
               {/*Services Block Four*/}
               {data && data.map((item , key) => (
+                <Link to='crypto-detail/'>
                 <div className="services-block-four how" key={key}>
                   <div className="inner-box">
                     <div className="step">{item.step}</div>
@@ -25,6 +28,7 @@ const SecHowItWorks = ({data , imgPhone}) => {
                     <div className="text">{item.description}</div>
                   </div>
                 </div>
+                </Link>
               ))}
             </div>
           </div>
